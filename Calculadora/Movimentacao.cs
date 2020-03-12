c8776d78f08401d2bd7fb97f90e70a0bdfcf5dae
Anderson Caetano Pilatti <anderson.pilatti@wises.com.br>
2020-03-12 10:42:42 -0300

    Add project files.

---
+++ Calculadora/Movimentacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Movimentacao
+    {
+        public DateTime Data { get; set; }
+        public string Tipo { get; set; } // C ou D
+        public Decimal Valor { get; set; }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Program
+    {
+        static List<Movimentacao> movimentacao = new List<Movimentacao>();
+        
+        static void Header(string area = null)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Clear();
+            Console.WriteLine("Banco CS");
+            
+            if (! string.IsNullOrWhiteSpace(area))
+            {
+                Console.WriteLine(area);
+            }
+
+            Console.WriteLine("");
+        }
+
+        static void ErrorMessage(string msg = null)
+        {
+            if (string.IsNullOrWhiteSpace(msg))
+                msg = "Entre com um valor numerico, pressione enter para continuar";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+
+        static void Menu()
+        {
+            Header();
+
+            Console.WriteLine("1 - Deposito");
+            Console.WriteLine("2 - Saque");
+            Console.WriteLine("3 - Extrato");
+            Console.WriteLine("4 - Saldo");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("");
+        }
+
+        static void Deposito()
+        {
+            Header("DEPOSITO");
+
+            try
+            {
+                Console.Write("Informe o valor a depositar ou digite 0 para sair: ");
+                decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+                if (valor == 0)
+                    return;
+
+                movimentacao.Add(new Movimentacao
+                {
+                    Data = DateTime.Now,
+                    Tipo = "C",
+                    Valor = valor
+                }) ;
+            }
+            catch (Exception)
+            {
+                ErrorMessage();
+                Deposito();
+            }
+
+            Console.Write("Informe o valor a depositar:");
+
+        }
+
+        static void Saque()
+        {
+            Header("SAQUE");
+
+            try
+            {
+                Console.Write("Informe o valor a sacar ou digite 0 para sair: ");
+                decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+                if (valor == 0)
+                    return;
+
+                movimentacao.Add(new Movimentacao
+                {
+                    Data = DateTime.Now,
+                    Tipo = "D",
+                    Valor = valor
+                });
+            }
+            catch (Exception)
+            {
+                ErrorMessage();
+                Deposito();
+            }
+
+            Console.Write("Informe o valor a depositar:");
+
+        }
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+
+            do
+            {
+                
+
+                Menu();
+
+                try
+                {
+                    Console.Write("Informe a opcao desejada: ");
+                    opcao = Convert.ToInt16(Console.ReadLine());
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            Deposito();
+                            break;
+
+                        case 2:
+                            Saque();
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    ErrorMessage();
+                    opcao = 100;
+                }
+
+            } while (opcao > 0);
+        }
+    }
+}
